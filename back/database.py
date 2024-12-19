3fc364b5e350c70d17704c3c020f52a892447883
shtalina <99807081+shtalina@users.noreply.github.com>
2024-12-19 12:42:45 +0700

    Update database.py

--- back/database.py
+++ back/database.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
  
 # создание движка
 engine = create_engine(
